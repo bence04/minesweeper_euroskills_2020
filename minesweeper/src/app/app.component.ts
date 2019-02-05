6d200627480c3bb6e3735d4489be9137d6c981c4
bence04 <tothbence95@gmail.com>
2019-02-05 13:52:30 +0100

    removed unused lines

--- minesweeper/src/app/app.component.ts
+++ minesweeper/src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { GameService } from './service/game.service';
 import { GameFieldModel, GameFieldEnum } from './model/game.enum';
-import { TouchSequence } from 'selenium-webdriver';
 import { timer, Subscription } from 'rxjs';
 
 @Component({
@@ -9,9 +8,7 @@ import { timer, Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'minesweeper';
-
+export class AppComponent {
   gameMap: GameFieldModel[][];
   endOfGame = false;
   timeInSec = 0;
@@ -20,22 +17,15 @@ export class AppComponent implements OnInit {
 
   constructor(private gameService: GameService) {}
 
-  ngOnInit() {
-    console.log('init');
-    this.newGame();
-  }
-
   newGame() {
     if (this.timerSubscription !== undefined) { this.timerSubscription.unsubscribe(); }
     this.gameMap = this.gameService.generateMap(9, 9, 10);
     this.allBombs = 10;
     this.endOfGame = false;
-    console.log(this.gameMap);
     this.timerSubscription = timer(0, 1000).subscribe((val) => this.timeInSec = val);
   }
 
   selectField(item: GameFieldModel, rowIndex: number, columnIndex: number) {
-    console.log('right click');
     if (!this.endOfGame) {
       if (item.isSelected) { this.allBombs++; } else { this.allBombs--; }
       this.gameMap[rowIndex][columnIndex].isSelected = !item.isSelected;
@@ -46,7 +36,6 @@ export class AppComponent implements OnInit {
   }
 
   clickField(item: GameFieldModel, rowIndex: number, columnIndex: number) {
-    console.log('left click');
     if (!this.endOfGame) {
       if (item.value === GameFieldEnum.BOMB) {
         this.gameFinnish();
@@ -54,7 +43,6 @@ export class AppComponent implements OnInit {
       } else {
         this.gameService.showEmptyNeighbours(rowIndex, columnIndex, this.gameMap);
       }
-
       if (this.gameService.isLastClick(this.gameMap)) {
         this.gameFinnish();
         alert('Nyertél');
@@ -71,6 +59,9 @@ export class AppComponent implements OnInit {
     this.timerSubscription.unsubscribe();
     this.endOfGame = true;
   }
+
+
+  /* localStorage.setItem('key', JSON.stringify()); */
 
 
 
