73dce5796ab88e071b9d7bdffb45745735bd73d1
bence04 <tothbence95@gmail.com>
2019-02-06 02:04:52 +0100

    start game login screen

--- minesweeper/src/app/app.component.ts
+++ minesweeper/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { GameService } from './service/game.service';
-import { GameFieldModel, GameFieldEnum, HighScoreModel } from './model/game.enum';
-import { timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,71 +6,7 @@ import { timer, Subscription } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  gameMap: GameFieldModel[][];
-  endOfGame = false;
-  timeInSec = 0;
-  allBombs = 0;
-  timerSubscription: Subscription;
-  highScore: HighScoreModel[] = [];
-
-  constructor(private gameService: GameService) {}
-
-  newGame() {
-    this.getHighscore();
-    if (this.timerSubscription !== undefined) { this.timerSubscription.unsubscribe(); }
-    this.gameMap = this.gameService.generateMap(9, 9, 10);
-    this.allBombs = 10;
-    this.endOfGame = false;
-    this.timerSubscription = timer(0, 1000).subscribe((val) => this.timeInSec = val);
-  }
-
-  selectField(item: GameFieldModel, rowIndex: number, columnIndex: number) {
-    if (!this.endOfGame) {
-      if (item.isSelected) { this.allBombs++; } else { this.allBombs--; }
-      this.gameMap[rowIndex][columnIndex].isSelected = !item.isSelected;
-    } else {
-      alert('kezdj új játékot');
-    }
-    return false;
-  }
-
-  clickField(item: GameFieldModel, rowIndex: number, columnIndex: number) {
-    if (!this.endOfGame) {
-      if (item.value === GameFieldEnum.BOMB) {
-        this.gameFinnish();
-        alert('vesztettél');
-      } else {
-        this.gameService.showEmptyNeighbours(rowIndex, columnIndex, this.gameMap);
-      }
-      if (this.gameService.isLastClick(this.gameMap)) {
-        this.gameFinnish();
-        this.highScore.push({name: 'asd', time: this.timeInSec});
-        const sortedHighScore = this.highScore.sort(function(e1, e2) {
-          return e1.time - e2.time;
-        }).slice(0, 5);
-        localStorage.setItem('highscores', JSON.stringify(sortedHighScore));
-        alert('Nyertél');
-      }
-    } else {
-      alert('kezdj új játékot');
-    }
-  }
-
-  gameFinnish() {
-    this.gameMap.map(e =>
-      e.map(el => el.isClicked = (el.value === GameFieldEnum.BOMB || el.isClicked))
-    );
-    this.timerSubscription.unsubscribe();
-    this.endOfGame = true;
-  }
-
-  getHighscore() {
-    const getScoreStr = localStorage.getItem('highscores');
-    if (getScoreStr !== null) {
-      this.highScore = JSON.parse(getScoreStr);
-    }
-  }
-
+  constructor() {}
 
 
 
